5c98a4c708ea7d0179829b3fdeb863bd499ad758
Geoff <Geoffrey.V.Herz@gmail.com>
2018-02-03 15:07:45 -0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,19 +1,20 @@
-document.body.onload = function() {
-  chrome.storage.sync.get("data", function(items) {
-    if (!chrome.runtime.error) {
-      console.log(items);
-      document.getElementById("data").innerText = items.data;
-    }
-  });
+//On popup open, search storage and populate the text fields with the previous inputs. 
+
+//On apply changes button, update all text input in the popup
+
+//Practice code
+function getUserName() {
+    var nameField = document.getElementById('name1');
+    var nameField2 = document.getElementById('text1');
+    var result = document.getElementById('result');
+
+    nameField2.defaultValue = "TESTING INPUT OVERWRITE";
+    result.textContent = 'Your username is: ' + nameField2.value;
+    chrome.storage.local.set({'textbox': nameField.value});
+    chrome.storage.local.get("textbox", function(getValue){
+        console.log("Get method sucessfull, obtained: " + getValue.textbox);
+    });
 }
 
-document.getElementById("set").onclick = function() {
-  var d = document.getElementById("text").value;
-    console.log(d);
-  chrome.storage.sync.set({ "data" : d }, function() {
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
-  });
-  window.close();
-}
+var subButton = document.getElementById('ApplyChanges');
+subButton.addEventListener('click', getUserName, false); 
