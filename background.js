d9747708b9ebad346680a5490240b37f3a2ce941
Geoffrey Herz <Geoffrey.V.herz@gmail.com>
2018-02-05 01:57:32 -0800

    Completed MVP
    Minimum qualifications set to be useful.
    Restrictions:
    -> Only 3 menus, no more or less.
    -> Clicking the "Apply Changes" button will not change the context menu,
    reloading the extension will fully apply the name change.
    
    Both of these missing features are planned to be added.

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
-//On recive message, change the title names of 
+var storage = chrome.storage.local;
+var allNames = [
+    "Default",
+    "Default",
+    "Default",
+    ]
 
+function main(){
+    storage.get('key', function(obj) {
+        allNames[0] = obj.key[0];
+        allNames[1] = obj.key[2];
+        allNames[2] = obj.key[4];
+        createContexMenus(allNames);
+    });
+}
 
-rightClickHandler = function(){ 
-    console.log("Right click registered, function activated");
-    copyToClipboard("Hello world!");
-    pastFromClipboard();
+function createContexMenus(names){
+    chrome.contextMenus.create({
+        title: names[0],
+        contexts:["editable"],
+        onclick: row1 
+      });
+
+      chrome.contextMenus.create({
+        title: names[1],
+        contexts:["editable"], 
+        onclick: row2 
+      });
+      
+      chrome.contextMenus.create({
+        title: names[2],
+        contexts:["editable"],  
+        onclick: row3 
+      });
 }
 
 function pastFromClipboard(){
@@ -22,20 +49,30 @@ function copyToClipboard (text) {
     document.body.removeChild(userString);
 }
 
-chrome.contextMenus.create({
-  title: "placehold 1",
-  contexts:["editable"],  // ContextType
-  onclick: rightClickHandler // A callback function
-});
+row1 = function(){ 
+    storage.get('key', function(obj) {
+        text = obj.key[1]
+        copyToClipboard(text);
+        pastFromClipboard();
+    });
+}
 
-chrome.contextMenus.create({
-  title: "placeholder 2",
-  contexts:["editable"], 
-  onclick: rightClickHandler 
-});
 
-chrome.contextMenus.create({
-  title: "Placeholder 3",
-  contexts:["editable"],  
-  onclick: rightClickHandler 
-});
+row2 = function(){ 
+    storage.get('key', function(obj) {
+        text = obj.key[3]
+        copyToClipboard(text);
+        pastFromClipboard();
+    });
+}
+
+
+row3 = function(){ 
+    storage.get('key', function(obj) {
+        text = obj.key[5]
+        copyToClipboard(text);
+        pastFromClipboard();
+    });
+}
+
+main();
