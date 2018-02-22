5edd428eda5d88a135fa1b23f373ada892c81f1c
Geoff <Geoffrey.V.Herz@gmail.com>
2018-02-22 11:41:31 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ Copyright (c) 2018 Geoffrey Herz
 */
 var storage = chrome.storage.local;
 var rowIDs = [];
-//This data construct is used to generate the storage object, it made itteration easier.
+//This data construct is used to generate the storage object.
 //rowIDs[n][x] = represents row n, each subarray x contains metadata on rows currently displayed on popup.html
 //rowIDs[][0] = key used to store row data in the storage object. 
 //rowIDs[][1] = elementID of the row's name input field. 
@@ -12,8 +12,8 @@ var rowIDs = [];
 
 //Start point
 window.onload = function() {
+     //loadDummyData();
     recoverStorage();
-    //loadDummyData();
 }
 
 //Debug test to display expected shape of a storage object.
@@ -43,7 +43,8 @@ function recoverStorage() {
     });
 }
 
-//Generates an empty row[index] complete with all event hooks ready
+//Generates one empty row at (index) complete with all event hooks ready. 
+//NOTE: Button is generated but not assigned in this function.
 function generateRow(index) {
     var table = document.getElementById("homescreen");
     var row = table.insertRow(-1);
@@ -58,7 +59,7 @@ function generateRow(index) {
     onTextInput(index);
 }
 
-//Stores the row meta information in rowID global array for later use when saving changes to storage.
+//Stores the row meta information in the rowID global array for later use when saving changes to storage.
 function updateRowIDs(index) {
     var nameID = document.getElementById("name" + index);
     var textID = document.getElementById("text" + index);
@@ -66,7 +67,7 @@ function updateRowIDs(index) {
     rowIDs.push(item);
 }
 
-//Creates a listener for the text field that will automatically update Data.
+//Creates a listener for the text fields on row(index)
 function onTextInput(index) {
     $("#name" + index)
         .on('change', modifyData);
@@ -74,8 +75,8 @@ function onTextInput(index) {
         .on('change', modifyData);
 }
 
-//Loops through all rowId's and saves them to storage.
-//Alerts the context menu there's been a change in storage.
+//Loops through all rowId's and saves name and text to storage.
+//Alerts the context menu there's been an update in storage.
 function modifyData() {
     var newObject = {}
     for (i in rowIDs) {
@@ -88,7 +89,7 @@ function modifyData() {
     chrome.runtime.sendMessage({});
 }
 
-//given an array of two strings (items), places them into respective text inputs on row[index]
+//given an array of two strings (items), places them into respective text inputs on the html row[index]
 function populateRow(items, index) {
     var name = document.getElementById('name' + index);
     var text = document.getElementById('text' + index);
